25e99a86ecda3fe7dd3179f59ea082800f2a53c0
Anno9474 <anno9474@gmail.com>
2020-10-12 00:16:03 +0200

    Day one Task two

--- 2015/DayOne.rb
+++ 2015/DayOne.rb
@@ -5,6 +5,25 @@ file = File.open('./input/input01.txt').read
 open = file.count '('
 closed = file.count ')'
 
-result = open - closed
+first_result = open - closed
 
-File.open("./output/output01.txt", "w+") { |f| f.write "#{ result }" }
+def calculate_floor(file)
+  file.chars.each_with_index.inject(0) do |floor, (character, index)|
+    if character == "(" ? (floor +=1) : (floor -= 1)
+      floor += 1
+    else
+      floor -= 1
+    end
+    if floor.negative?
+      puts index + 1
+      break
+    end
+    floor
+  end
+end
+
+second_result = calculate_floor(file)
+File.open("./output/output01.txt", "w+") { |f| f.write "This is the first result: #{ first_result } \nThis is the second result: #{ second_result }" }
+
+
+
